15be8014c9ccf30a2b9688058676c8f806dfcc34
Nguyen Nam <thenam97ls@gmail.com>
2023-01-09 22:37:39 +0700

    ngFor_ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,18 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   public name='Nguyễn Nam'
   public age = 26
+  public traiCay = ['tao','nho','cam','quyt']
+ public traiCay2 = [
+  {ten:'tao',gia:12, hagia:true},
+  {ten:'nho', gia:-15, hagia:false}
+]
+
   constructor(){}
+
+  public ngOnInit():void{
+    console.log('trai cay = ', this.traiCay);
+  }
+
   public resetName(): void {
     this.name=''
   }
